d449e848b558d5c681c1d9d030b2d6940ff70d0f
alvar <alvar@DESKTOP-52OFONO>
2021-12-20 23:01:27 +0000

    Kata 6 Version 1

---
+++ src/toys/SerialNumberGenerator.java
@@ -0,0 +1,12 @@
+package toys;
+
+public class SerialNumberGenerator {
+    private Integer serialNumber = 0;
+    
+    public Integer next(){
+        return this.serialNumber++;
+        
+    }
+    
+    
+}
